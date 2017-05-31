d50384908d15f83cb85113bd9adf2decb7bef304
Gianni Moschini <leibowitz@users.noreply.github.com>
2017-05-31 19:55:58 +0100

    Add files via upload

--- run_multiprocess.sh
+++ run_multiprocess.sh
@@ -1,104 +1,57 @@
-
-SCRIPT_DIR=`dirname ${0}`
-
-OUTPUT_DIRECTORY='' # default
-
-
-MAX_PROCESSES=2
-
-
-
-
-if [ $# -gt 0 ] ; then
-	
-
-	LAST_ARG="${@: -1}"
-	if [ -d "$LAST_ARG" ] ; then
-		OUTPUT_DIRECTORY="$LAST_ARG"
-	fi
-
-	ERROR_DIRECTORY="$OUTPUT_DIRECTORY"'/errors'
-	SUCCESS_DIRECTORY="$OUTPUT_DIRECTORY"'/success'
-	LOG_DIR="$OUTPUT_DIRECTORY"'/logs'
-
-	if [ ! -d "$ERROR_DIRECTORY" ] ; then
-		echo $ERROR_DIRECTORY" is not a directory"
-		exit 1
-	fi
-	if [ ! -d "$SUCCESS_DIRECTORY" ] ; then
-		echo $SUCCESS_DIRECTORY" is not a directory"
-		exit 1
-	fi
-	if [ ! -d "$LOG_DIR" ] ; then
-		echo $LOG_DIR" is not a directory"
-		exit 1
-	fi
-	
-	declare -a PIDS
-
-	wait_jobs ()
-	{
-		for PID in ${!PIDS[@]} ; do
-			FILE=${PIDS[$PID]}
-			NAME=`basename $FILE`
-			echo -n "Wait Job: "$PID" file " $NAME
-			wait $PID
-			RET=$?
-			if [ $RET -eq 0 ] ; then
-				echo " [OK]"
-				cp "$FILE" "$SUCCESS_DIRECTORY"/$NAME
-			else
-				echo " [FAILED]"
-				cp "$FILE" "$ERROR_DIRECTORY"/$NAME
-			fi
-
-			unset PIDS[$PID]
-		done
-	}
-
-	echo "Load $# files and move them to $OUTPUT_DIRECTORY"
-
-	while [ $# -gt 0 ] ; do
-		FILE="$1"
-		if [ -f "$FILE" ] ; then
-			NAME=`basename $FILE`
-			LOG_FILE="$LOG_DIR"/"$NAME"".log"
-			echo "Processing $NAME "
-			# Run your command with FILE here
-			echo "$FILE" > "$LOG_FILE" 2>&1 &
-
-			PID=$!
-			
-			PIDS[$PID]="$FILE"
-			
-			#wait $PID
-			#RET=$?
-			
-			NBJOBS=`jobs -p | wc -l`
-			if [ $NBJOBS -ge $MAX_PROCESSES ] ; then
-				wait_jobs
-			fi
-			
-		else
-			echo $FILE" is not a file"
-		fi
-
-		if [ $# -eq 2 -a -d "$2" ] ; then
-			#last arg is directory, skip it
-			echo "Done"
-			break
-		fi
-
-		shift
-	done
-
-	wait_jobs 
-	
-
-	exit 0
-else
-	echo "Sorry I need two arguments"
-	exit 1
+if [ $# -lt 3 ] ; then
+    echo "Usage: $0 [REPEAT] [PROCESSES] [CMD]"
+    exit
 fi
 
+REPEAT=${1}
+MAX_PROCESSES=${2}
+CMD=${3}
 
+declare -a PIDS
+
+wait_jobs ()
+{
+    for PID in ${!PIDS[@]} ; do
+        CMD="${PIDS[$PID]}"
+        echo -n "Wait Job: "$PID" CMD " $CMD
+        wait $PID
+        RET=$?
+        if [ $RET -eq 0 ] ; then
+            echo " [OK]"
+        else
+            echo " [FAILED]"
+        fi
+
+        unset PIDS[$PID]
+        # only wait for one job at a time
+        break
+    done
+}
+
+
+
+for X in `seq 1 $REPEAT`; do
+    echo "Processing $CMD "
+    # Run your command here
+    time $CMD >/dev/null &
+
+    PID=$!
+
+    PIDS[$PID]="$CMD"
+    
+
+    #wait $PID
+    #RET=$?
+
+    # Pause if necessary, to avoid running more than MAX_PROCESSES
+    
+    while [ `jobs -p | wc -l` -ge $MAX_PROCESSES ] ; do
+        wait_jobs
+    done
+done
+    
+# finish by waiting for all jobs to end
+wait_jobs 
+
+
+exit 0
